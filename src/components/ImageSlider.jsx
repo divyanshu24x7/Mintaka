189fe7460c49a3bbb1b97869fd2ba9debb349b0b
iCaran <pratapmorgan@gmail.com>
2024-09-03 03:12:20 +0530

    improve carousel

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const ImageSlider = ({ imageUrls, interval = 3000 }) => {
   const [imageIndex, setImageIndex] = useState(0);
+  const sliderRef = useRef(null);
+
+  const images = [...imageUrls, imageUrls[0]];
 
   useEffect(() => {
-    if (imageUrls.length === 0) return; // No images to slide
+    const handleTransitionEnd = () => {
+      if (imageIndex === images.length - 1) {
+        sliderRef.current.style.transition = 'none';
+        setImageIndex(0);
+        sliderRef.current.getBoundingClientRect();
+        setTimeout(() => {
+          sliderRef.current.style.transition = 'transform 0.9s ease'; // Slightly faster transition
+        }, 50);
+      }
+    };
+
+    sliderRef.current.addEventListener('transitionend', handleTransitionEnd);
 
     const intervalId = setInterval(() => {
-      setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-      let temp = imageUrls[2];
-    }, interval);
+      setImageIndex((prevIndex) => {
+        if (prevIndex === images.length - 1) {
+          return 0; // Reset immediately when reaching the duplicated image
+        } else {
+          return prevIndex + 1;
+        }
+      });
+    }, interval * (imageIndex === images.length - 1 ? 0.7 : 1)); // Reduce interval on reset
 
-    return () => clearInterval(intervalId); // Clear interval on component unmount
-  }, [imageUrls.length, interval]);
+    return () => {
+      sliderRef.current.removeEventListener('transitionend', handleTransitionEnd);
+      clearInterval(intervalId);
+    };
+  }, [imageIndex, images.length, interval]);
 
   return (
     <div className='w-full h-full relative overflow-hidden'>
       <div
+        ref={sliderRef}
         className='w-full h-full flex transition-transform duration-1000'
         style={{
-          transform: `translateX(${-100 * imageIndex}%)` // Adjust for duplicated images
-
+          transform: `translateX(${-100 * imageIndex}%)`,
         }}
       >
-        {imageUrls.map((url) => (
+        {images.map((url, index) => (
           <img
-            key={url}
+            key={index}
             src={url}
             className='img-slider-img'
+            alt={`Slide ${index}`}
           />
         ))}
       </div>
