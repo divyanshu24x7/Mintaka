d539881b76206437ed4fd6802e69c5b6123f3218
divyanshu <divyanshukumar725@gmail.com>
2024-09-06 00:12:02 +0530

    added search icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import ImageSlider from './ImageSlider'
-import slide1 from '../assets/images/img1.jpg'
-import slide2 from '../assets/images/img2.png'
-import slide3 from '../assets/images/img3.png'
+import React from 'react';
+import ImageSlider from './ImageSlider';
+import slide1 from '../assets/images/img1.jpg';
+import slide2 from '../assets/images/img2.png';
+import slide3 from '../assets/images/img3.png';
+import { FaSearch } from 'react-icons/fa'; // Import search icon
 
-const IMAGES = [slide1,slide2,slide3]
+const IMAGES = [slide1, slide2, slide3];
+
 const Hero = () => {
   return (
-    <div className='max-w-[1440px] w-full aspect-[10/2] mx-auto my-0 relative'>
-        <ImageSlider imageUrls={IMAGES} />
-        <div className="absolute left-1/2 top-3/1 transform -translate-x-1/2 -translate-y-1/2 w-full p-2">
-        <input 
-          type="text" 
-          className="w-[60%] h-14 mx-auto p-2 shadow-md border border-gray-300 focus:ring focus:ring-blue-500 outline-none block"
-          placeholder="Search name..." 
-        />
+    <div className="max-w-[1440px] w-full aspect-[10/2] mx-auto my-0 relative">
+      {/* Image Slider */}
+      <ImageSlider imageUrls={IMAGES} />
+
+      {/* Positioned Input */}
+      <div className="absolute left-1/2 top-3/1 transform -translate-x-1/2 -translate-y-1/2 w-full p-2">
+        <div className="relative w-[60%] mx-auto">
+          <input 
+            type="text" 
+            className="w-full h-14 rounded-md pl-12 pr-4 py-2 shadow-md border border-gray-300 focus:ring focus:ring-blue-500 outline-none block"
+            placeholder="Search..." 
+          />
+          <FaSearch 
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+            size={20} // Adjust size as needed
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
